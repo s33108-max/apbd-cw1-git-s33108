7b78ac4c36f1a29d86161be894675273a06bd510
Klaudia Rutkowska <s33108@pjwstk.edu.pl>
2026-03-18 13:20:06 +0100

    Initial project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace apbd_cw1_git_s33108
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
